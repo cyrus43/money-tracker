261de559335ae536bedd35b6751701f10aa2f185
Cyrus Mbithi <cyrusmbithi92@gmail.com>
2023-02-08 11:09:21 +0300

    add models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Wallet } from './wallet.entity';
+import { WalletService } from './wallet.service';
+import { WalletController } from './wallet.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'money-tracker',
+      entities: [Wallet],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Wallet]),
+  ],
+  providers: [WalletService],
+  controllers: [WalletController],
 })
 export class AppModule {}
