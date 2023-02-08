1749360196e0b10015c4b71dec56318df8e4431f
Cyrus Mbithi <cyrusmbithi92@gmail.com>
2023-02-08 11:27:43 +0300

    modify main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.connectMicroservice({
-    strategy: new TypeOrmModule({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'money-tracker',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-  });
-  await app.startAllMicroservicesAsync();
   await app.listen(3000);
 }
 bootstrap();
