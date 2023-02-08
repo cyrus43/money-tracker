46dba83688619949fc717e6edc7380c9d424b9d4
Cyrus Mbithi <cyrusmbithi92@gmail.com>
2023-02-08 16:20:25 +0300

    Modify transactions controller

---
+++ src/controllers/transactions.controller.ts
@@ -0,0 +1,35 @@
+/* eslint-disable prettier/prettier */
+// src/app/controllers/transactions.controller.ts
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { Transaction } from 'src/interfaces/transaction.interface';
+import { TransactionsService } from 'src/services/transaction.service';
+
+@Controller('transactions')
+export class TransactionsController {
+  constructor(private readonly transactionsService: TransactionsService) {}
+
+  @Post()
+  async create(@Body() transaction: Transaction) {
+    return await this.transactionsService.create(transaction);
+  }
+
+  @Get()
+  async findAll() {
+    return await this.transactionsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return await this.transactionsService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() transaction: Transaction) {
+    return await this.transactionsService.update(id, transaction);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return await this.transactionsService.delete(id);
+  }
+}
