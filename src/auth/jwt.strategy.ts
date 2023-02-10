8e7e87808023a5fc434ee6685c1f5972ca6f367a
Cyrus Mbithi <cyrusmbithi92@gmail.com>
2023-02-10 12:34:44 +0300

    Add auth module

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/models/user.model';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectModel('User.name') private userModel: Model<User>,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'secretKey',
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await this.userModel.findById(payload.sub);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
